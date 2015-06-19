78104213a88ea57494219d734efd04ae18d1ee5d
JuneLeGency <lichen900210@gmail.com>
2015-06-19 18:22:49 +0800

    init

---
+++ aidlmoudle/src/main/aidl/com/legency/aidl/aidlmoudle/IMoudleMyAidlInterface.aidl
@@ -0,0 +1,13 @@
+// IMoudleMyAidlInterface.aidl
+package com.legency.aidl.aidlmoudle;
+
+// Declare any non-default types here with import statements
+
+interface IMoudleMyAidlInterface {
+    /**
+     * Demonstrates some basic types that you can use as parameters
+     * and return values in AIDL.
+     */
+    void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat,
+            double aDouble, String aString);
+}
---
+++ app/src/main/java/com/legency/aidl/testaidl/MainActivity.java
@@ -0,0 +1,48 @@
+package com.legency.aidl.testaidl;
+
+import android.os.RemoteException;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.legency.aidl.aidlmoudle.IMoudleMyAidlInterface;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    IMoudleMyAidlInterface.Stub m = new IMoudleMyAidlInterface.Stub(){
+
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
